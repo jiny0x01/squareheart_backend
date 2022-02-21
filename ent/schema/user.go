1648e835f84c2e513a3b179dea2148fa81ecc863
Jinyoung Kim <sdk159147@gmail.com>
2022-02-22 00:44:15 +0900

    user schema 뼈대 작성

---
+++ ent/schema/user.go
@@ -0,0 +1,59 @@
+package schema
+
+import (
+	"errors"
+	"time"
+	"unicode/utf8"
+
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema/field"
+	"github.com/twinj/uuid"
+)
+
+func MaxRuneCount(maxLen int) func(s string) error {
+	return func(s string) error {
+		if utf8.RuneCountInString(s) > maxLen {
+			return errors.New("value is more than the max length")
+		}
+		return nil
+	}
+}
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("uuid", uuid.UUID{}).
+			Default(uuid.New),
+		field.String("email").
+			Unique().
+			// Match("").
+			NotEmpty(),
+		field.String("nickname").
+			Unique().
+			NotEmpty().
+			// Match("").
+			Annotations(entsql.Annotation{
+				Size: 12,
+			}).
+			Validate(MaxRuneCount(12)),
+		field.String("password").
+			Unique().
+			NotEmpty().
+			Sensitive(),
+		field.Bool("active").
+			Default(true),
+		field.Time("created_at").
+			Default(time.Now),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
