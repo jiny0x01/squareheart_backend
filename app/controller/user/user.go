074067bc2ee6d922843867b516ad318a33c54190
Kim Jinyoung <sdk159147@gmail.com>
2022-03-15 00:45:41 +0900

    Appending JWT

--- app/controller/user/user.go
+++ app/controller/user/user.go
@@ -4,25 +4,32 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	util "github.com/jiny0x01/storylink_backend/app/internal"
 	"github.com/jiny0x01/storylink_backend/app/model"
 )
 
 func SignUp(c *fiber.Ctx) error {
-	var dto model.SignUpDTO
-	if err := c.BodyParser(&dto); err != nil {
-		log.Fatalln(err)
-		return err
+	dto := new(model.SignUpDTO)
+	if err := c.BodyParser(dto); err != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(err.Error())
 	}
-	log.Printf("email: %s\n", dto.Email)
-	log.Printf("userId: %s\n", dto.Nickname)
-	log.Printf("password: %s\n", dto.Password)
-	err := model.CreateUser(c, &dto)
+	log.Println(dto)
+	err := model.CreateUser(c, dto)
 	if err != nil {
-		log.Fatalln(err)
-		return err
+		return c.Status(fiber.StatusConflict).JSON(err.Error())
 	}
 
-	// TODO
-	// transfer token
-	return c.SendString("It will be return token")
+	// change dto.Email to uuid
+	t, err := util.GenereateToken(dto.Email)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
+	}
+
+	// regist token to redis
+	util.RegistToken()
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"token": t})
+}
+
+func SignIn(c *fiber.Ctx) error {
+
 }
