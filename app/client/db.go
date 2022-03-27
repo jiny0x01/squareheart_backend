52a8c48269936602bd55c0abbc3f806b2988d91d
Kim Jinyoung <sdk159147@gmail.com>
2022-03-27 23:38:07 +0900

    change redis version v7 to v8

--- app/client/db.go
+++ app/client/db.go
@@ -2,7 +2,9 @@ package client
 
 import (
 	"context"
+	"os"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/jiny0x01/storylink_backend/ent"
 	_ "github.com/lib/pq"
 )
@@ -10,6 +12,7 @@ import (
 type DB struct {
 	Client *ent.Client
 	Ctx    context.Context
+	Redis  *redis.Client
 }
 
 var db DB
@@ -21,4 +24,19 @@ func SetDB(d *DB) {
 
 func GetDB() *DB {
 	return &db
+}
+
+func init() {
+	//Initializing redis
+	dsn := os.Getenv("REDIS_DSN")
+	if len(dsn) == 0 {
+		dsn = "localhost:6379"
+	}
+	db.Redis = redis.NewClient(&redis.Options{
+		Addr: dsn, //redis port
+	})
+	_, err := db.Redis.Ping(db.Ctx).Result()
+	if err != nil {
+		panic(err)
+	}
 }
