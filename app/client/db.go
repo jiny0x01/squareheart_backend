3711970a924190e9dfd68217f86183914ec5f4a4
Kim Jinyoung <sdk159147@gmail.com>
2022-03-28 01:41:08 +0900

    Migrate initialization db

--- app/client/db.go
+++ app/client/db.go
@@ -2,10 +2,13 @@ package client
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/jiny0x01/storylink_backend/ent"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
@@ -27,7 +30,34 @@ func GetDB() *DB {
 }
 
 func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error db loading .env file")
+		return
+	}
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s TimeZone=Asia/Seoul",
+		os.Getenv("HOST"),
+		os.Getenv("PORT"),
+		os.Getenv("STORYLINK_USER"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("STORYLINK_PW"),
+		os.Getenv("SSL_MODE"))
+	log.Printf("db connnection str:%s\n", connStr)
+
+	dbClient, err := ent.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	db.Client = dbClient
+	db.Ctx = context.Background()
+
+	// auto migration
+	if err := db.Client.Schema.Create(db.Ctx); err != nil {
+		panic(err)
+	}
 	//Initializing redis
+
 	dsn := os.Getenv("REDIS_DSN")
 	if len(dsn) == 0 {
 		dsn = "localhost:6379"
@@ -35,7 +65,7 @@ func init() {
 	db.Redis = redis.NewClient(&redis.Options{
 		Addr: dsn, //redis port
 	})
-	_, err := db.Redis.Ping(db.Ctx).Result()
+	_, err = db.Redis.Ping(db.Ctx).Result()
 	if err != nil {
 		panic(err)
 	}
--- main.go
+++ main.go
@@ -1,53 +1,19 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"os"
-
 	"github.com/jiny0x01/storylink_backend/app/client"
 	controller "github.com/jiny0x01/storylink_backend/app/controller/user"
-	"github.com/jiny0x01/storylink_backend/ent"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error db loading .env file")
-		return
-	}
 
-	connStr := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s TimeZone=Asia/Seoul",
-		os.Getenv("HOST"),
-		os.Getenv("PORT"),
-		os.Getenv("STORYLINK_USER"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("STORYLINK_PW"),
-		os.Getenv("SSL_MODE"))
-	log.Printf("db connnection str:%s\n", connStr)
-	db, err := ent.Open("postgres", connStr)
-	if err != nil {
-		panic(err)
-		return
-	}
-	defer db.Close()
-	ctx := context.Background()
-	dbClient := client.DB{
-		Client: db,
-		Ctx:    ctx,
-	}
-	client.SetDB(&dbClient)
-
-	// auto migration
-	if err := db.Schema.Create(dbClient.Ctx); err != nil {
-		log.Fatalf("Failed creating schema resources: %v", err)
-		return
-	}
+	db := client.GetDB()
+	defer db.Client.Close()
+	defer db.Redis.Close()
 	app := fiber.New()
+	//	app.Use(middleware.JWTAuth)
 	app.Post("/signup", controller.SignUp)
 
 	app.Listen(":8080")
